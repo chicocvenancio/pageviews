f5bc544c7d542d8bffb65938b6f67469e62b5910
MusikAnimal <musikanimal@gmail.com>
2017-09-27 23:51:30 -0400

    squash me

---
+++ public_html/mediaviews/faq/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php include '../../_head.php'; ?>
+    <title>
+      <?php echo $I18N->msg( 'siteviews-title' ) . ' - ' . $I18N->msg( 'faq' ); ?>
+    </title>
+  </head>
+  <body class="<?php echo $rtl; ?>">
+    <?php $currentApp = 'siteviews'; ?>
+    <?php include '../../_header.php'; ?>
+    <div class="container">
+      <header class="col-lg-12 text-center">
+        <h4>
+          <strong>
+            <?php echo $I18N->msg( 'siteviews-title' ); ?>
+          </strong>
+          <small class="app-description">
+            <?php echo $I18N->msg( 'faq' ); ?>
+          </small>
+        </h4>
+      </header>
+      <main class="col-lg-10 col-lg-offset-1">
+        <ul class="list-group">
+          <?php include "../../faq_parts/_old_data.php"; ?>
+          <?php include "../../faq_parts/_todays_data.php"; ?>
+          <li class="list-group-item" id="metric">
+            <p>
+              <strong><?php echo $I18N->msg( 'faq-source-title' ); ?></strong>
+            </p>
+            <ul class="faq-ul">
+              <li>
+                <i><?php echo $I18N->msg( 'pageviews' ); ?></i>
+                <?php echo $I18N->msg( 'faq-source-pageviews' ); ?>
+              </li>
+              <li>
+                <i><?php echo $I18N->msg( 'unique-devices' ); ?></i>
+                <?php echo $I18N->msg( 'faq-source-unique-devices' ); ?>
+              </li>
+              <li>
+                <i><?php echo $I18N->msg( 'pagecounts-legacy' ); ?></i>
+                <?php echo $I18N->msg( 'faq-source-pagecounts' ); ?>
+              </li>
+            </ul>
+          </li>
+          <?php include "../../faq_parts/_counts.php"; ?>
+          <li class="list-group-item" id="agent">
+            <dt>agent</dt>
+            <dd>
+              <?php echo $I18N->msg( 'url-structure-agent-no-bots', array( 'variables' => array( '<code>user</code>', '<code>spider</code>', '<code>all-agents</code>' ), 'parsemag' => true ) ); ?>
+            </dd>
+          </li>
+          <?php include "../../faq_parts/_date_dot.php"; ?>
+          <?php include "../../faq_parts/_chart_type.php"; ?>
+          <li class="list-group-item">
+            <p>
+              <strong><?php echo $I18N->msg( 'faq-top-viewed-title' ); ?></strong>
+            </p>
+            <p>
+              <?php echo $I18N->msg( 'faq-try-tool', array( 'variables' => array( "<a href='/topviews'>" . $I18N->msg( 'topviews-title' ) . "</a>" ), 'parsemag' => true ) ); ?>
+            </p>
+          </li>
+          <?php include "../../faq_parts/_feedback.php"; ?>
+        </ul>
+        <div class="col-lg-12 text-center tm">
+          <a href="/siteviews">
+            <?php echo $I18N->msg( 'faq-return-to', array( 'variables' => array( $I18N->msg( 'siteviews-title' ) ), 'parsemag' => true ) ); ?>
+          </a>
+        </div>
+      </main>
+      <?php include "../../_footer.php"; ?>
+    </div>
+  </body>
+</html>
